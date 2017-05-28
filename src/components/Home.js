251b11c17c8867bd8550ba2fdea0a53d6ab8b1a2
glhrmv <gvmgui@gmail.com>
2017-05-28 01:12:34 +0100

    Change InfoBox contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
 const Home = () => (
-  <div className="card">
-    <div className="card-header">
+  <CSSTransitionGroup
+    transitionName="fade"
+    transitionAppear={true}
+    transitionAppearTimeout={500}
+    transitionEnter={false}
+    transitionLeave={false}>
+    <div className="card">
+      <div className="card-header">
         <p className="card-header-title">
           Weatherise
         </p>
+      </div>
+      <div className="card-content content">
+        <blockquote>
+          make (a house or other building) resistant to cold weather, typically by adding insulation.
+        </blockquote>
+
+        <p>Find the current weather and a 7 day forecast for anywhere in the world!</p>
+        <p>You can click on one of the suggestions to the left to get started.</p>
+      </div>
     </div>
-    <div className="card-content content">
-      <blockquote>
-        make (a house or other building) resistant to cold weather, typically by adding insulation.
-      </blockquote>
-    </div>
-  </div>
+  </CSSTransitionGroup>
+
 );
 
 export default Home;
--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
 import SearchBox from './SearchBox';
 import InfoBox from './InfoBox';
 
 const LeftMenu = () => (
   <div>
-    <SearchBox />
-    <InfoBox />
+    <CSSTransitionGroup
+      transitionName="fade"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <SearchBox />
+      <InfoBox />
+    </CSSTransitionGroup>
   </div>
 );
 
