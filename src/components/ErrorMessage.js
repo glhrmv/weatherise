964f509f971ddc3fd9f9fd09455ac18a53d3c252
dogui <gvm.gui@gmail.com>
2017-05-30 19:34:01 +0100

    Add http warning, make forecast text a bit smaller

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorMessage = () => (
+  <article className="message is-dark">
+    <div className="message-body">
+      Something went wrong with the request. Please try again later.
+      &nbsp;
+      <a onClick={() => {window.location.reload()}}>Try again now?</a>
+    </div>
+  </article>
+);
+
+export default ErrorMessage;
